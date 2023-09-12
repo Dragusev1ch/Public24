bb696791c7835b64168f5090f54309bb57962717
Nazar <nazar.drahusevych@gmail.com>
2023-09-12 21:09:24 +0300

    Add account, branch, customer, loan, transaction entities

---
+++ DataAccessLayer/Entities/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Branch
+    {
+        public int BranchId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
---
+++ DataAccessLayer/Entities/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }  = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Card { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
